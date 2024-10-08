21ad3daf1a40b293f1b1163f95dd462ff20ece9c
QuangThuan <tranquanthuan132@gmail.com>
2024-10-09 00:22:55 +0700

    add/socket

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,11 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import SwaggerOption from "./configs/swagger";
 import swaggerUi from 'swagger-ui-express';
 import morgan from "morgan";
+import http from 'http';
+import { Server, Socket } from 'socket.io';
 import cors from 'cors';
 const app = express();
+const httpServer = http.createServer(app);
 const port = process.env.PORT || 3000;
 const apiRoute = express.Router();
 const swaggerDocument = swaggerJSDoc(SwaggerOption);
@@ -24,6 +27,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 Database.connect();
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000', // Địa chỉ client của bạn
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+});
+io.on('connection', (socket: Socket) => {
+  console.log(`Một người dùng đã kết nối:  ${socket.id}`);
+
+  socket.on('chat message', (msg: string) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Một người dùng đã ngắt kết nối');
+  });
+});
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/users', useRouteUser());
 app.use('/patients', useRoutePatient());
@@ -35,6 +57,6 @@ app.use('/complaints', useRouteComplaint());
 app.use('/services', useServiceRouter());
 app.use("/setting-appointment", UseSettingAppoitmentRoutes());
 app.use("/api/v1", apiRoute);
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
