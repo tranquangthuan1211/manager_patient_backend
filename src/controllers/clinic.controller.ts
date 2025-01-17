22ff0f730214bff862c6fdb5ac8fc60747be5bb3
QuangThuan <tranquanthuan132@gmail.com>
2025-01-17 17:09:22 +0700

    update

--- src/controllers/clinic.controller.ts
+++ src/controllers/clinic.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import UsersDataBase from "../models/user-model";
 import ClinicDataBase from "../models/clinic.model";
+import { ObjectId } from "mongodb";
 
 class ClinicController {
   async getClinics(req: Request, res: Response) {
@@ -15,6 +16,7 @@ class ClinicController {
       res.status(500).json({ message: error.message });
     }
   }
+
   async getDoctors(req: Request, res: Response) {
     try {
       const doctors = await UsersDataBase.users.find({ id_clinic:req.params.id }).toArray();
@@ -22,6 +24,18 @@ class ClinicController {
         error: 0,
         message: "success",
         data: doctors,
+      });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+  async getClinic(req: Request, res: Response) {
+    try {
+      const clinic = await ClinicDataBase.clinic.findOne({ _id: new ObjectId(req.params.id) });
+      res.status(200).json({
+        error: 0,
+        message: "success",
+        data: clinic,
       });
     } catch (error: any) {
       res.status(500).json({ message: error.message });
